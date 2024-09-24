024cbe95c735b5353ee30e15b3288583def6605a
Chason Du Xiaocheng <383225059@qq.com>
2024-09-24 12:58:22 +0800

    Update test_unsigned_variable_underflow.cpp

--- test_unsigned_variable_underflow.cpp
+++ test_unsigned_variable_underflow.cpp
@@ -3,6 +3,22 @@ void test_print_counter()
     unsigned int counter = 5;
     while (counter >= 0) {
        IMS_DLOG(IMS_LOGLOW, "Counter: %u\n", counter);
-        counter--;
+       counter--;
     }
 }
+
+class LinkNodeManager {
+    private:
+        unsigned int mNumOfLinkNode;
+
+    public:
+        LinkNodeManager(unsigned int numOfLinkNode) : mNumOfLinkNode(numOfLinkNode) {}
+
+        void getWeights(unsigned short *weights) {
+    		int i = 0
+            while (i < mNumOfLinkNode - 1) {
+                weights[i] = 0;
+                i++;
+            }
+        }
+};
